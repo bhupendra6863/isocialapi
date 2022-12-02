69e5fdd20efc62fa93605766a5efd639edc08c01
bhupendra singh <bpsingh6863@gmail.com>
2022-12-02 16:00:00 +0530

    updated index

--- index.js
+++ index.js
@@ -65,5 +65,5 @@ if(process.env.NODE_ENV=="production"){
 }
 
 app.listen(PORT, () => {
-  console.log(`Backend server is running at ${PORT}`);
+  console.log(`Backend server is running at ${PORT}!`);
 });

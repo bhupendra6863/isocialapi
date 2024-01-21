ae16fd2714254565ec190f9c2edd632cba95dbf1
bhupendra6863 <bhupendrasingh6863@gmail.com>
2024-01-21 18:34:09 +0530

    type error

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.use("/api/conversations", conversationRoute);
 app.use("/api/messages", messageRoute);
 
 if(process.env.NODE_ENV=="production"){
-  app.use(express.ststic("client/build"));
+  app.use(express.static("client/build"));
   const path = require("path");
   app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
